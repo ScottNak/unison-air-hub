a54f518dce5b5287d92fd2064e0302e55f38b13d
ScottNak <scottnak@gmail.com>
2026-09-11 17:53:41 -0700

    Rebuild best_teams and spotlight (bt v14, sp v15)

--- page_versions.js
+++ page_versions.js
@@ -2,6 +2,6 @@ window.UNIAIR_PAGE_VERSIONS = {
   "catalog": "v10",
   "songs": "v7",
   "kizuna": "v5",
-  "bestteams": "v13",
-  "spotlight": "v14"
+  "bestteams": "v14",
+  "spotlight": "v15"
 };
