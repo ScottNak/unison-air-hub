7f9ebfafb2e2d0a41c21708c2e7f074b84c3fbd1
ScottNak <scottnak@gmail.com>
2026-09-19 05:39:42 -0700

    Regenerate: spotlight v20, best teams v21

--- page_versions.js
+++ page_versions.js
@@ -2,6 +2,6 @@ window.UNIAIR_PAGE_VERSIONS = {
   "catalog": "v11",
   "songs": "v8",
   "kizuna": "v5",
-  "bestteams": "v20",
-  "spotlight": "v19"
+  "bestteams": "v21",
+  "spotlight": "v20"
 };
